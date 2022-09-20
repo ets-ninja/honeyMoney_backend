119817c2168f427611e395d85c84e95a734bb68b
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-09-20 17:49:19 +0300

    Adds morgan middleware

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-require('dotenv').config();
+const morganMiddleware = require("./middlewares/morgan.middleware");
+const logger = require("./services/logger");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morganMiddleware);
 
 const server = app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  logger.info(`Server started on port ${PORT}`);
 });
