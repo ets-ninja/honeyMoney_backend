7548c4b10aa52669c73b477746ecaff83ee36edc
v.shevtsov <versaptr@gmail.com>
2022-09-20 19:01:30 +0300

    Added mongoose, updated package, gitignore

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 const app = express();
+
+mongoose.connection.on('open', ()=> {
+    console.log('MongoDB connected');
+});
+
+mongoose.connection.on('error', (err)=> {
+    console.log(err);
+});
 
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+const server = http.createServer(app);
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
+  server.listen(PORT, err => {
+    if (err) {
+      console.err(err);
+      return;
+    }
+    console.log(`Server listens on port ${PORT}`);
+  });
+}
+
+startServer();
