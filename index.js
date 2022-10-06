e52b4e146933a15a58652ad2481fbaa93607d0f8
ValeriaRbk <60627710+ValeriaRbk@users.noreply.github.com>
2022-10-06 13:10:49 +0300

    Init payment system (#18)
    * not ready yet
    
    * add payment routes and endpoints, add stripe id to user model
    
    * add endpoints payment intent, transactions history
    
    * add last4 and authenticate routes
    
    * add swagger docs
    
    * Delete .vscode directory

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const wishlistRoutes = require('./routes/wishlist.routes');
 const docsRoute = require('./routes/api-docs.routes');
+const payRoutes = require('./routes/payment.routes');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/wishlist', wishlistRoutes);
 app.use('/api/api_docs', docsRoute);
+app.use('/api/payment', payRoutes);
 
 // 404 Route should be at the end of all routes
 app.use((req, res, next) => {
@@ -64,4 +66,4 @@ async function startServer() {
   });
 }
 
-startServer();
+startServer();  
---
+++ routes/payment.routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const paymentController = require('../controllers/payment.controllers');
+
+router.post(
+  '/setup_secret',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.newSetupIntent,
+);
+
+router.post(
+  '/payment_secret',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.newPaymentIntent,
+);
+
+router.get(
+  '/usercards',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.getCustomerCards,
+);
+
+router.get(
+  '/userbalance',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.getCustomerBalance,
+);
+
+router.post(
+  '/transaction_new',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.newTransaction,
+);
+
+router.get(
+  '/transaction_history',
+  passport.authenticate('jwt', { session: false }),
+  paymentController.transactionsHistory,
+);
+
+module.exports = router;
