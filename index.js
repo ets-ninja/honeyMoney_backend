e82a31ad24660e8849d2bfbf280203623dc8b11c
Parker <antonkozak98@gmail.com>
2022-10-09 15:17:41 +0200

    added layout share-bank

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const HttpError = require('./utils/http-error');
 const passport = require('./middlewares/passport.middleware');
 const morganMiddleware = require('./middlewares/morgan.middleware');
 const logger = require('./services/logger');
-
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
 // Consts
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
@@ -16,17 +17,32 @@ const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const wishlistRoutes = require('./routes/wishlist.routes');
 const docsRoute = require('./routes/api-docs.routes');
+const bank = require('./routes/bank.routes');
 
 const app = express();
+
+const hbs = expressHandlebars.create({
+  defaultLayout: 'main',
+  extname: 'handlebars',
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.static('public'));
 
 app.use(cors());
 app.use(passport.initialize());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(morganMiddleware);
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/wishlist', wishlistRoutes);
 app.use('/api/api_docs', docsRoute);
+app.use('/api/bank', bank);
 
 // 404 Route should be at the end of all routes
 app.use((req, res, next) => {
