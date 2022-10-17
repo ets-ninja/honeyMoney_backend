f692f898e44184b5c68c111d07dfde7ff7f1593c
Nikita Pelenskyi <67456353+npelenskii@users.noreply.github.com>
2022-10-17 14:04:48 +0300

    before merge with dev

--- controllers/basket.controllers.js
+++ controllers/basket.controllers.js
@@ -193,6 +193,8 @@ async function createBasket(req, res, next) {
 
   let basket;
 
+  //ObjectId('63453e83ded011f11f79c181')
+
   try {
     basket = await Basket.create({
       ownerId: req.user._id,
