2361d6bba6ba2393c0cefe52074f30b718e9983c
Parker <antonkozak98@gmail.com>
2022-10-25 06:57:23 +0200

    update public/index.js

--- public/index.js
+++ public/index.js
@@ -193,11 +193,11 @@ async function checkStatus() {
 function showMessage(messageText) {
   const messageContainer = document.querySelector('#payment-message');
 
-  messageContainer?.classList.remove('hidden');
-  messageContainer?.textContent = messageText;
+  messageContainer.classList.remove('hidden');
+  messageContainer.textContent = messageText;
 
   setTimeout(function () {
-    messageContainer?.classList.add('hidden');
+    messageContainer.classList.add('hidden');
     messageText.textContent = '';
   }, 4000);
 }
